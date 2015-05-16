5e2444ea133b2533f86515cf79de39b6517c4b41
Craig Wann <craigwann1@gmail.com>
2015-05-15 23:59:35 -0500

    Adding blogView.

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+$(window).load(function(){
+
+});
+
+function process_content(item) {
+
+    $(item).find('.blog-view').each(function(index, el) {
+        new BlogView(el);
+    });
+
+}
+
+$(document).ready(function() {
+    process_content($('body'));
+});
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp   = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+var lib    = require('bower-files')();
+
+gulp.task('default', function () {
+    //Build bower
+    gulp.src(lib.ext('js').files)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('assets'));
+    gulp.src(lib.ext('css').files)
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest('assets'));
+
+    //Combine bower with files
+    gulp.src([
+        'assets/vendor.js',
+        'assets/blogView.js',
+        'assets/main.js'
+    ])
+        .pipe(concat('all.js'))
+        .pipe(uglify({
+            mangle: false,
+            compress: false
+   }))
+    .pipe(gulp.dest('assets'));
+
+    gulp.src([
+        'assets/main.css'
+        ]).pipe(concat('all.css'))
+          .pipe(minify())
+          .pipe(gulp.dest('assets'));
+});
