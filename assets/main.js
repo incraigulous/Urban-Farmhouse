7babc8ce5fc680e4cffa5512f9dedc0a5af3ea90
Craig Wann <craigwann1@gmail.com>
2015-05-19 22:05:43 -0500

    Integrating font awesome and adding instagram and vimeo social options.

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,19 @@ function process_content(item) {
     $(item).find('.blog-view').each(function(index, el) {
         new BlogView(el);
     });
+
+    $(item).find('.slider').each(function(index, el) {
+        $(el).slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 7000
+        });
+
+    });
 }
 
 $(document).ready(function() {
