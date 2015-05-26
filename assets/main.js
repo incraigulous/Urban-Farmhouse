0d52766c9e27e8a59f4cac987ba260a9ff6e65ae
Craig Wann <craigwann1@gmail.com>
2015-05-25 22:49:25 -0500

    Adding gallery thumbnails to home page. Fixing slider load issues.

--- assets/main.js
+++ assets/main.js
@@ -3,15 +3,45 @@ $(window).load(function(){
 });
 
 function process_content(item) {
-
     $(item).find('.blog-view').each(function(index, el) {
         new BlogView(el);
     });
 
     $(item).find('.slider').each(function(index, el) {
-        $(el).on('setPosition', function(event, slick, direction){
+
+        function resetCaptionPositions() {
+            $(el).find('.caption').each(function () {
+                $(this).css('margin-top', -500);
+            });
+        }
+
+        resetCaptionPositions();
+
+        function showCaptionBox(currentSlide) {
+            resetCaptionPositions();
+            var caption = $(currentSlide).find('.caption');
+            caption.animate({
+                    'margin-top': 40
+                }, 'slow');
+        };
+
+        $(el).on('init', function(event, slick, direction){
+            var height = $('#slider-preloader').height();
+            //while (height < 1) {
+            //    var height = $('#slider-preloader').height();
+            //}
+            slick.$list.css('height',height);
+            if (slick.$list.height() === 0) {
+                alert('zero');
+            }
             $('#slider-preloader').hide();
-            $(el).fadeIn();
+            $(el).fadeIn(500, function () {
+                showCaptionBox(slick.$slides[slick.currentSlide]);
+            });
+        });
+
+        $(el).on('afterChange', function(event, slick, currentSlide){
+            showCaptionBox(slick.$slides[currentSlide]);
         });
 
         $(el).slick({
